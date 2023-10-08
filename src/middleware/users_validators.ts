76c0002954ab8628305c31a14fc25850ccc39b39
Stanislav <gsv9026@mail.ru>
2023-10-08 14:34:30 +0300

    555

--- src/middleware/users_validators.ts
+++ src/middleware/users_validators.ts
@@ -2,5 +2,5 @@ import { body } from "express-validator";
 
 export const loginValid = body('login').isString().trim().isLength({min: 3, max:10}).matches('^[a-zA-Z0-9_-]*$')
 export const passwordValid = body('password').isString().trim().isLength({min: 6, max:20})
-export const emailValid = body('email').isString().trim().isLength({min: 1, max:30}).matches('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+export const emailValid = body('email').isString().trim().isLength({min: 1, max:30}).isEmail()
 
